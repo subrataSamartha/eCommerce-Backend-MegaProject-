841a0e5df976e8c65d3d77e277ffc7014f55c36b
Sunrata Samartha <subrata.samartha2019@gift.edu.in>
2022-12-23 11:27:30 +0530

    created methods in user schema for token and comare password

--- models/user.schema.js
+++ models/user.schema.js
@@ -3,6 +3,7 @@ import AuthRoles from "../utils/authRoles";
 import bcrypt from "bcryptjs";
 import JWT from "jsonwebtoken";
 import crypto from "crypto"; // no need to install this library. It is available by default in NodeJs
+import config from "../config/index";
 
 const userSchema = mongoose.Schema(
   {
@@ -46,5 +47,29 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
+
+/**
+ * add more featuers directly to your schema
+ */
+userSchema.methods = {
+  //compare password
+  comparePassword: async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  },
+
+  //generate JWT token
+  getJWTToken: function () {
+    return JWT.sign(
+      {
+        _id: this._id,
+        role: this.role,
+      },
+      config.JWT_SECRET,
+      {
+        expiredIn: config.JWT_EXPIRY,
+      }
+    );
+  },
+};
 
 export default mongoose.model("User", userSchema); //im mongoose the name will be "Users"
