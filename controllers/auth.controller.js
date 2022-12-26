6ef470b672db2a4178dcb739dc6539228a6c4538
Sunrata Samartha <subrata.samartha2019@gift.edu.in>
2022-12-27 01:03:10 +0530

    Auth controller - login and logout added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -46,3 +46,61 @@ export const signUp = asyncHandler(async (req, res) => {
     user,
   });
 });
+
+/*******************************************************************************************************************
+ * @LOGIN
+ * @route http://localhost:4000/api/auth/login
+ * @description User signIn controller for loging user
+ * @parameters email, password
+ * @return User Object
+ *
+ *****************************************************************************************************************/
+
+export const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomError("please fill all fields", 400);
+  }
+
+  const user = User.findOne({ email }).select("+password");
+
+  if (!user) {
+    throw new CustomError("Invalid credentials", 400);
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (isPasswordMatched) {
+    const token = user.getJWTToken();
+    user.password = undefined;
+    res.cookie("token", token, cookieOptions);
+    return res.status(200).json({
+      success: true,
+      token,
+      user,
+    });
+  }
+
+  throw new CustomError("INvalid credentials", 400);
+});
+
+/*******************************************************************************************************************
+ * @LOGOUT
+ * @route http://localhost:4000/api/auth/logout
+ * @description User logout by clearing user cookies
+ * @parameters N/A
+ * @return success message
+ *
+ *****************************************************************************************************************/
+export const logout = asyncHandler(async (_req, res) => {
+  // res.clearCookie()
+  res.cookie("token", null, {
+    expires: new Date(Date.now),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    message: "Logged Out",
+  });
+});
