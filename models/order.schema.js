c54c622221690f9bcd4cb7fef6ebbd8bb874d939
Sunrata Samartha <subrata.samartha2019@gift.edu.in>
2022-12-26 13:29:37 +0530

    order schema added to model

---
+++ models/order.schema.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    product: {
+      type: [
+        {
+          productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+            required: true,
+          },
+          count: Number,
+          price: Number,
+        },
+      ],
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    coupon: String,
+    transactionId: String,
+    status: {
+      type: String,
+      enum: ["ORDERD", "SHIPPED", "DELIVERED", "CANCELLED"],
+      default: "ORDERED",
+      //can we improve this
+    },
+    //paymentMode: UPI, creditcard or wallet, COD
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Order", orderSchema);
