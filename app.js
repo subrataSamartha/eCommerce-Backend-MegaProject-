d9b716480b55521491a43b7b2b4ef7fa9f1f1d66
Sunrata Samartha <subrata.samartha2019@gift.edu.in>
2022-12-26 18:52:22 +0530

    added morgan and express middleware

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //for using form data
+app.use(cors());
+app.use(xookieParser());
+
+//morgan logger
+app.use(morgan("tiny"));
 
 export default app;
