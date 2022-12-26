7c8d80b4cac7eb9185c550231355973a65deb637
Sunrata Samartha <subrata.samartha2019@gift.edu.in>
2022-12-26 16:29:57 +0530

    design methods for async handler and error handler

---
+++ services/asyncHandler.js
@@ -0,0 +1,12 @@
+const asyncHandler = (func) => async (req, res, next) => {
+  try {
+    await func(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export default asyncHandler;
---
+++ utils/customError.js
@@ -0,0 +1,8 @@
+class CostomError extends Error {
+  constructor(message, code) {
+    super(message);
+    this.code = code;
+  }
+}
+
+export default CostomError;
